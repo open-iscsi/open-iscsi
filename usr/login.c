8f80de66247c1b4f21a865736de6444c66f8380d
dmitry_yus <dmitry_yus@d7303112-9cec-0310-bdd2-e83a94d6c2b6>
2005-10-20 14:49:59 +0000

    Erez Zilber: An error should be returned only if conn->max_recv_dlength != strtoul(value, NULL, 0). In the current version, an error code is returned if session->provider->rdma != 0 (which is not ok).
    git-svn-id: svn://svn.berlios.de/open-iscsi@424 d7303112-9cec-0310-bdd2-e83a94d6c2b6

--- usr/login.c
+++ usr/login.c
@@ -551,11 +551,12 @@ get_op_params_text_keys(iscsi_session_t *session, int cid,
 	} else if (iscsi_find_key_value("InitiatorRecvDataSegmentLength", text,
 					end, &value, &value_end)) {
 		if (session->provider->rdma) {
-			if (conn->max_recv_dlength != strtoul(value, NULL, 0))
+			if (conn->max_recv_dlength != strtoul(value, NULL, 0)) {
 				log_error("Login negotiation failed, "
 					  "InitiatorRecvDataSegmentLength wasn't "
 					  "accepted by the target");
-			return LOGIN_NEGOTIATION_FAILED;
+				return LOGIN_NEGOTIATION_FAILED;
+			}
 		}
 		text = value_end;
 	} else if (iscsi_find_key_value("TargetRecvDataSegmentLength", text,
