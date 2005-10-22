604b9da049398f81d7ed486c14f8f6587c9700a2
aaizman <aaizman@d7303112-9cec-0310-bdd2-e83a94d6c2b6>
2005-10-22 21:47:07 +0000

    Fixed compile w/gcc-2.96
    git-svn-id: svn://svn.berlios.de/open-iscsi@426 d7303112-9cec-0310-bdd2-e83a94d6c2b6

--- usr/log.c
+++ usr/log.c
@@ -188,26 +188,27 @@ int log_dequeue (void * buff)
 	struct logmsg * src = (struct logmsg *)la->head;
 	struct logmsg * dst = (struct logmsg *)buff;
 	struct logmsg * lst = (struct logmsg *)la->tail;
+	int len;
 
 	if (la->empty)
 		return 1;
 
-	int len = strlen((char *)&src->str) * sizeof(char) +
-		  sizeof(struct logmsg) + 1;
+	len = strlen((char *)&src->str) * sizeof(char) +
+	      sizeof(struct logmsg) + 1;
 
 	dst->prio = src->prio;
 	memcpy(dst, src,  len);
 
 	if (la->tail == la->head)
-		la->empty = 1; /* we purge the last logmsg */
+		la->empty = 1; /* purge the last log msg */
 	else {
 		la->head = src->next;
 		lst->next = la->head;
 	}
 	logdbg(stderr, "dequeue: %p, %p, %i, %s\n",
-		(void *)src, src->next, src->prio, (char *)&src->str);
+	       (void *)src, src->next, src->prio, (char *)&src->str);
 
-	memset((void *)src, 0,  len);
+	memset((void *)src, 0, len);
 
 	return la->empty;
 }
