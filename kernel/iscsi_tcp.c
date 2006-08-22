7367ad824b64226280db4af9f839b72f2bf1fb7e
mnc <mnc@d7303112-9cec-0310-bdd2-e83a94d6c2b6>
2006-08-22 19:55:12 +0000

    merged wrong patch. fix compile leak
    git-svn-id: svn://svn.berlios.de/open-iscsi@662 d7303112-9cec-0310-bdd2-e83a94d6c2b6

--- kernel/iscsi_tcp.c
+++ kernel/iscsi_tcp.c
@@ -1827,6 +1827,7 @@ static void
 iscsi_tcp_conn_stop(struct iscsi_cls_conn *cls_conn, int flag)
 {
 	struct iscsi_conn *conn = cls_conn->dd_data;
+	struct iscsi_tcp_conn *tcp_conn = conn->dd_data;
 
 	iscsi_conn_stop(cls_conn, flag);
 	iscsi_tcp_release_conn(conn);
