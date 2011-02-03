702c60cc964209df5ad004615aaf40662650b0b6
Mark Rustad <mark.d.rustad@intel.com>
2011-02-02 17:54:47 -0800

    Add dcb_app.h for DCB support
    Add header file with prototypes for functions in dcb_app.c.
    
    Signed-off-by: Mark Rustad <mark.d.rustad@intel.com>

---
+++ usr/dcb_app.h
@@ -0,0 +1,34 @@
+/*******************************************************************************
+
+  DCB application support
+  Copyright(c) 2007-2010 Intel Corporation.
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms and conditions of the GNU General Public License,
+  version 2, as published by the Free Software Foundation.
+
+  This program is distributed in the hope it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+  more details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+
+  The full GNU General Public License is included in this distribution in
+  the file called "COPYING".
+
+  Contact Information:
+  e1000-eedc Mailing List <e1000-eedc@lists.sourceforge.net>
+  Intel Corporation, 5200 N.E. Elam Young Parkway, Hillsboro, OR 97124-6497
+
+*******************************************************************************/
+
+#ifndef _DCB_APP_H_
+#define _DCB_APP_H_
+
+int get_dcb_app_pri_by_port(const char *iface, int port);
+int get_dcb_app_pri_by_ethtype(const char *iface, int ethtype);
+
+#endif  /* _DCB_APP_H_ */
