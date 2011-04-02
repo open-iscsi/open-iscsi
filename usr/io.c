fcd08dfbd02a279ffa69618a5216f6cf4f88e1da
Mike Christie <michaelc@cs.wisc.edu>
2011-04-02 05:17:04 -0500

    iscsi tools: Don't try to bind offload EPs to sockets
    If discovery offload is not supported iscsiadm/iscsid
    drops down to normal socket based discovery. The
    problem is that io.c will try to bind the session
    to the socket if the iface has binding info, but
    for offload cards we will not be able to bind
    based on the hw address. This is because that will
    info is private to the iscsi function and not exposed
    to the network layer.

--- usr/io.c
+++ usr/io.c
@@ -299,6 +299,9 @@ static int bind_conn_to_iface(iscsi_conn_t *conn, struct iface_rec *iface)
 {
 	struct iscsi_session *session = conn->session;
 
+	if (strcmp(iface->transport_name, DEFAULT_TRANSPORT))
+		return 0;
+
 	memset(session->netdev, 0, IFNAMSIZ);
 	if (iface_is_bound_by_hwaddr(iface) &&
 	    net_get_netdev_from_hwaddress(iface->hwaddress, session->netdev)) {
