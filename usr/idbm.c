b8bb7bac9566300c27067c12aa995e573d2f56e5
Hannes Reinecke <hare@suse.de>
2014-11-14 11:29:56 -0800

    Allow modifications for iface.gateway and iface.subnet_mask
    For proper operations we need to set gateway and subnet_mask
    of the individual interfaces. Not every target is connected
    to the local network.

--- usr/idbm.c
+++ usr/idbm.c
@@ -262,6 +262,8 @@ idbm_recinfo_node(node_rec_t *r, recinfo_t *ri)
 	__recinfo_str(IFACE_IPADDR, ri, r, iface.ipaddress, IDBM_SHOW, num, 1);
 	__recinfo_str(IFACE_ISCSINAME, ri, r, iface.name, IDBM_SHOW, num, 1);
 	__recinfo_str(IFACE_NETNAME, ri, r, iface.netdev, IDBM_SHOW, num, 1);
+	__recinfo_str(IFACE_GATEWAY, ri, r, iface.gateway, IDBM_SHOW, num, 1);
+	__recinfo_str(IFACE_SUBNET_MASK, ri, r, iface.subnet_mask, IDBM_SHOW, num, 1);
 	/*
 	 * svn 780 compat: older versions used node.transport_name and
 	 * rec->transport_name
@@ -283,10 +285,6 @@ idbm_recinfo_node(node_rec_t *r, recinfo_t *ri)
 	if (iface_type == ISCSI_IFACE_TYPE_IPV4) {
 		__recinfo_str(IFACE_BOOT_PROTO, ri, r, iface.bootproto,
 			      IDBM_SHOW, num, 1);
-		__recinfo_str(IFACE_SUBNET_MASK, ri, r, iface.subnet_mask,
-			      IDBM_SHOW, num, 1);
-		__recinfo_str(IFACE_GATEWAY, ri, r, iface.gateway, IDBM_SHOW,
-			      num, 1);
 		__recinfo_str(IFACE_DHCP_ALT_CID, ri, r,
 			      iface.dhcp_alt_client_id_state, IDBM_SHOW,
 			      num, 1);
