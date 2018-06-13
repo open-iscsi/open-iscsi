4bf7a9898f9536a0a8a32c94e6e6555da18b2391
Chris Leech <cleech@redhat.com>
2018-06-13 08:08:27 -0700

    vlan setting sync across ipv4/ipv6 for be2iscsi
    be2iscsi exports two ifaces per host port for ipv4 and ipv6 network
    configurations. But, they need to have the same link level configuration
    including vlan settings. If vlan setting are modified in only one iface
    record, then whichever record is applied last (filesystem dependant I
    think) will take effect and things may not work.
    
    This change to iscsiadm applies vlan updates to all records with
    matching MAC addresses if a flag is set on the transport.
    The new transport flag is only set for be2iscsi.

--- usr/transport.c
+++ usr/transport.c
@@ -91,6 +91,7 @@ struct iscsi_transport_template bnx2i = {
 struct iscsi_transport_template be2iscsi = {
 	.name		= "be2iscsi",
         .bind_ep_required = 1,
+	.sync_vlan_settings = 1,
 	.create_conn	= be2iscsi_create_conn,
 	.ep_connect	= ktransport_ep_connect,
 	.ep_poll	= ktransport_ep_poll,
