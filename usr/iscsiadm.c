open-iscsi/open-iscsi

commit 87ea50a1c3a97abd5d223bd7f8530109062f1239

libopeniscsiusr: Add node query support
 * New API functions:
    * iscsi_nodes_get()
    * iscsi_nodes_free()
    * And other node property query functions.

 * Replaced the node query functions used by `iscsiadm -m node` command
   which then generate identical output like old one.

 * Tested for back compatibility on old style node file:
        /etc/iscsi/nodes/<target_name>/<address>,<port>
   But only by renaming existing node file as I failed to find any
   version of iscsiadm are generating that style of node file.

 * API version increased to 0.2.0 due to API addition.

Signed-off-by: Gris Ge <fge@redhat.com>

usr/iscsiadm.c
@@ -29,6 +29,7 @@
 #include <signal.h>
 #include <time.h>
 #include <sys/stat.h>
+#include <inttypes.h>
 
 #include <libopeniscsiusr/libopeniscsiusr.h>
 
@@ -675,26 +676,122 @@ static int login_portals(struct node_rec *pattern_rec)
 	return rc;
 }
 
-static int print_nodes(int info_level, struct node_rec *rec)
+static void print_node_flat(struct iscsi_node *node)
 {
-	struct node_rec tmp_rec;
+		printf("%s,%" PRIu16 "%s\n",
+		       iscsi_node_portal_get(node),
+		       iscsi_node_tpgt_get(node),
+		       iscsi_node_target_name_get(node));
+}
+
+static void print_nodes_tree(struct iscsi_node **nodes, uint32_t node_count)
+{
+	uint32_t i = 0;
+	struct iscsi_node *cur_node = NULL;
+	struct iscsi_node *pre_node = NULL;
+
+	// According to libopeniscsiusr document, nodes are sorted. There
+	// is no need to create hash table for this.
+	for (i = 0; i < node_count; ++i) {
+		cur_node = nodes[i];
+		if ( i != 0)
+			pre_node = nodes[i - 1];
+		if ((pre_node == NULL) ||
+		    (strcmp(iscsi_node_target_name_get(cur_node),
+			    iscsi_node_target_name_get(pre_node)) != 0))
+			printf("Target: %s\n",
+			       iscsi_node_target_name_get(cur_node));
+		if ((pre_node == NULL) ||
+		    (strcmp(iscsi_node_conn_address_get(cur_node),
+			    iscsi_node_conn_address_get(pre_node)) != 0) ||
+		    (iscsi_node_conn_port_get(cur_node) !=
+		     iscsi_node_conn_port_get(pre_node)))
+			printf("\tPortal: %s,%d\n",
+			       iscsi_node_portal_get(cur_node),
+			       iscsi_node_tpgt_get(cur_node));
+		if ((pre_node == NULL) ||
+		    (strcmp(iscsi_node_iface_name_get(cur_node),
+			    iscsi_node_iface_name_get(pre_node)) != 0))
+			printf("\t\tIface Name: %s\n",
+			       iscsi_node_iface_name_get(cur_node));
+	}
+}
+
+static int print_nodes(struct iscsi_context *ctx, int info_level)
+{
+	struct iscsi_node **nodes = NULL;
+	uint32_t node_count = 0;
+	uint32_t i = 0;
 	int rc = 0;
 
-	switch (info_level) {
-	case 0:
-	case -1:
-		rc = for_each_matched_rec(rec, NULL, idbm_print_node_flat);
-		break;
-	case 1:
-		memset(&tmp_rec, 0, sizeof(node_rec_t));
-		rc = for_each_matched_rec(rec, &tmp_rec,
-					  idbm_print_node_and_iface_tree);
-		break;
-	default:
+	if ((info_level != 0) && (info_level != -1) && (info_level != 1)) {
 		log_error("Invalid info level %d. Try 0 or 1.", info_level);
 		rc = ISCSI_ERR_INVAL;
+		goto out;
 	}
 
+	rc = iscsi_nodes_get(ctx, &nodes, &node_count);
+	if (rc != LIBISCSI_OK)
+		goto out;
+
+	if (info_level == 1)
+		print_nodes_tree(nodes, node_count);
+	else
+		for (i = 0; i < node_count; ++i)
+			print_node_flat(nodes[i]);
+
+out:
+	iscsi_nodes_free(nodes, node_count);
+	return rc;
+}
+
+static int print_nodes_config(struct iscsi_context *ctx, bool show_secret,
+			      const char *target_name, const char *address,
+			      int32_t port, const char *iface_name)
+{
+	int rc = 0;
+	struct iscsi_node **nodes = NULL;
+	struct iscsi_node *node = NULL;
+	uint32_t node_count = 0;
+	uint32_t i = 0;
+	bool match = false;
+	bool has_match = false;
+
+	rc = iscsi_nodes_get(ctx, &nodes, &node_count);
+	if (rc != LIBISCSI_OK)
+		return rc;
+
+	for (i = 0; i < node_count; ++i) {
+		node = nodes[i];
+		match = true;
+		if ((target_name != NULL) &&
+		    (strlen(target_name) != 0) &&
+		    (strcmp(target_name,
+			    iscsi_node_target_name_get(node)) != 0))
+			match = false;
+		if ((address != NULL) &&
+		    (strlen(address) != 0) &&
+		    (strcmp(address, iscsi_node_conn_address_get(node)) != 0))
+			match = false;
+		if ((port != -1) && (port != iscsi_node_conn_port_get(node)))
+			match = false;
+		if ((iface_name != NULL) &&
+		    (strlen(iface_name) != 0) &&
+		    (strcmp(iface_name, iscsi_node_iface_name_get(node)) != 0))
+			match = false;
+
+		if (match == true) {
+			iscsi_node_print_config(node, show_secret);
+			has_match = true;
+		}
+	}
+
+	iscsi_nodes_free(nodes, node_count);
+
+	if (has_match == false) {
+		log_error("No records found");
+		rc = ISCSI_ERR_NO_OBJS_FOUND;
+	}
 	return rc;
 }
 
@@ -2560,8 +2657,8 @@ static int verify_node_params(struct list_head *params, struct node_rec *rec)
 }
 
 /* TODO cleanup arguments */
-static int exec_node_op(int op, int do_login, int do_logout,
-			int do_show, int do_rescan, int do_stats,
+static int exec_node_op(struct iscsi_context *ctx, int op, int do_login,
+			int do_logout, int do_show, int do_rescan, int do_stats,
 			int info_level, struct node_rec *rec,
 			struct list_head *params)
 {
@@ -2601,9 +2698,10 @@ static int exec_node_op(int op, int do_login, int do_logout,
 	}
 
 	if ((!do_login && !do_logout && op == OP_NOOP) &&
-	    (!strlen(rec->name) && !strlen(rec->conn[0].address) &&
-	     !strlen(rec->iface.name))) {
-		rc = print_nodes(info_level, rec);
+	    ((rec == NULL) ||
+	     (!strlen(rec->name) && !strlen(rec->conn[0].address) &&
+	      !strlen(rec->iface.name)))) {
+		rc = print_nodes(ctx, info_level);
 		goto out;
 	}
 
@@ -2623,7 +2721,9 @@ static int exec_node_op(int op, int do_login, int do_logout,
 	}
 
 	if (op == OP_NOOP || (!do_login && !do_logout && op == OP_SHOW)) {
-		rc = for_each_matched_rec(rec, &do_show, idbm_print_node_info);
+		rc = print_nodes_config(ctx, do_show ? true : false,
+					rec->name, rec->conn[0].address,
+					rec->conn[0].port, rec->iface.name);
 		goto out;
 	}
 
@@ -3690,7 +3790,7 @@ main(int argc, char **argv)
 			goto out;
 		}
 
-		rc = exec_node_op(op, do_login, do_logout, do_show,
+		rc = exec_node_op(ctx, op, do_login, do_logout, do_show,
 				  do_rescan, do_stats, info_level, rec,
 				  &params);
 		break;
@@ -3775,7 +3875,7 @@ main(int argc, char **argv)
 			}
 
 			/* drop down to node ops */
-			rc = exec_node_op(op, do_login, do_logout, do_show,
+			rc = exec_node_op(ctx, op, do_login, do_logout, do_show,
 					  do_rescan, do_stats, info_level,
 					  rec, &params);
 free_info:
@@ -3789,7 +3889,7 @@ free_info:
 				goto out;
 			}
 			if (do_logout || do_rescan || do_stats) {
-				rc = exec_node_op(op, do_login, do_logout,
+				rc = exec_node_op(ctx, op, do_login, do_logout,
 						 do_show, do_rescan, do_stats,
 						 info_level, NULL, &params);
 				goto out;

.gitignore
@@ -11,3 +11,4 @@ libopeniscsiusr/docs/man/
 libopeniscsiusr/tests/test_context
 libopeniscsiusr/tests/test_session
 libopeniscsiusr/tests/test_iface
+libopeniscsiusr/tests/test_node

libopeniscsiusr/Makefile
@@ -26,7 +26,7 @@ PKGCONF_DIR ?= $(LIB_DIR)/pkgconfig
 LIBISCSI_USR_DIR=$(TOPDIR)/libopeniscsiusr
 
 LIBISCSI_USR_VERSION_MAJOR=0
-LIBISCSI_USR_VERSION=0.1.1
+LIBISCSI_USR_VERSION=0.2.0
 SONAME=$(LIBISCSI_USR_VERSION)
 DEVLIB = libopeniscsiusr.so
 LIBS = $(DEVLIB).$(SONAME)
@@ -36,10 +36,10 @@ HEADERS = libopeniscsiusr/libopeniscsiusr.h \
 	  libopeniscsiusr/libopeniscsiusr_common.h \
 	  libopeniscsiusr/libopeniscsiusr_session.h \
 	  libopeniscsiusr/libopeniscsiusr_iface.h
-TESTS = tests/test_context tests/test_session tests/test_iface
+TESTS = tests/test_context tests/test_session tests/test_iface tests/test_node
 EXTRA_MAN_FILES = libopeniscsiusr.h.3
 
-OBJS = context.o misc.o session.o sysfs.o iface.o idbm.o
+OBJS = context.o misc.o session.o sysfs.o iface.o idbm.o node.o default.o
 
 CFLAGS ?= -O2 -g
 CFLAGS += -Wall -Werror -Wextra -fvisibility=hidden -fPIC

libopeniscsiusr/default.c
@@ -1,0 +1,108 @@
+/*
+ * Copyright (C) 2017-2018 Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Gris Ge <fge@redhat.com>
+ */
+
+#include <string.h>
+
+#include "libopeniscsiusr/libopeniscsiusr.h"
+#include "default.h"
+#include "iface.h"
+#include "node.h"
+
+#define CONFIG_DIGEST_NEVER	0
+
+static void default_session_op_cfg(struct iscsi_session_op_cfg *op_cfg)
+{
+	op_cfg->InitialR2T = 0;
+	op_cfg->ImmediateData = 1;
+	op_cfg->FirstBurstLength = DEF_INI_FIRST_BURST_LEN;
+	op_cfg->MaxBurstLength = DEF_INI_MAX_BURST_LEN;
+	op_cfg->DefaultTime2Wait = ISCSI_DEF_TIME2WAIT;
+	op_cfg->DefaultTime2Retain = 0;
+	op_cfg->MaxConnections = 1;
+	op_cfg->MaxOutstandingR2T = 1;
+	op_cfg->ERL = 0;
+	op_cfg->FastAbort = 1;
+}
+
+static void default_conn_op_cfg(struct iscsi_conn_op_cfg *op_cfg)
+{
+	op_cfg->MaxXmitDataSegmentLength = 0;
+	op_cfg->MaxRecvDataSegmentLength = DEF_INI_MAX_RECV_SEG_LEN;
+	op_cfg->HeaderDigest = DIGEST_NEVER;
+	op_cfg->DataDigest = DIGEST_NEVER;
+	op_cfg->IFMarker = 0;
+	op_cfg->OFMarker = 0;
+}
+
+/*
+ * default is to use tcp through whatever the network layer
+ * selects for us with the /etc/iscsi/initiatorname.iscsi iname.
+ */
+static void default_iface(struct iscsi_iface *iface)
+{
+	snprintf(iface->transport_name,
+		 sizeof(iface->transport_name)/sizeof(char),
+		 DEFAULT_TRANSPORT);
+
+	if (!strlen(iface->name))
+		snprintf(iface->name, sizeof(iface->name)/sizeof(char),
+			 DEFAULT_IFACENAME);
+}
+
+void _default_node(struct iscsi_node *node)
+{
+	node->tpgt = PORTAL_GROUP_TAG_UNKNOWN;
+	node->disc_type = DISCOVERY_TYPE_STATIC;
+	node->leading_login = 0;
+	node->session.cmds_max = CMDS_MAX;
+	node->session.xmit_thread_priority = XMIT_THREAD_PRIORITY;
+	node->session.initial_cmdsn = 0;
+	node->session.queue_depth = QUEUE_DEPTH;
+	node->session.nr_sessions = 1;
+	node->session.initial_login_retry_max = DEF_INITIAL_LOGIN_RETRIES_MAX;
+	node->session.reopen_max = 32;
+	node->session.auth.authmethod = 0;
+	node->session.auth.password_length = 0;
+	node->session.auth.password_in_length = 0;
+	node->session.err_tmo.abort_timeout = DEF_ABORT_TIMEO;
+	node->session.err_tmo.lu_reset_timeout = DEF_LU_RESET_TIMEO;
+	node->session.err_tmo.tgt_reset_timeout = DEF_TGT_RESET_TIMEO;
+	node->session.err_tmo.host_reset_timeout = DEF_HOST_RESET_TIMEO;
+	node->session.tmo.replacement_timeout = DEF_REPLACEMENT_TIMEO;
+	node->session.se = NULL;
+	node->session.sid = 0;
+	node->session.multiple = 0;
+	node->session.scan = DEF_INITIAL_SCAN;
+
+	default_session_op_cfg(&node->session.op_cfg);
+
+	node->conn.startup = ISCSI_STARTUP_MANUAL;
+	node->conn.port = ISCSI_DEFAULT_PORT;
+	node->conn.tcp.window_size = TCP_WINDOW_SIZE;
+	node->conn.tcp.type_of_service = 0;
+	node->conn.tmo.login_timeout= DEF_LOGIN_TIMEO;
+	node->conn.tmo.logout_timeout= DEF_LOGOUT_TIMEO;
+	node->conn.tmo.auth_timeout = 45;
+	node->conn.tmo.noop_out_interval = DEF_NOOP_OUT_INTERVAL;
+	node->conn.tmo.noop_out_timeout = DEF_NOOP_OUT_TIMEO;
+
+	default_conn_op_cfg(&node->conn.op_cfg);
+
+	default_iface(&node->iface);
+}

libopeniscsiusr/default.h
@@ -1,0 +1,73 @@
+/*
+ * Copyright (C) 2017-2018 Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Gris Ge <fge@redhat.com>
+ */
+
+#ifndef __ISCSI_USR_DEFAULT_H__
+#define __ISCSI_USR_DEFAULT_H__
+
+#include "libopeniscsiusr/libopeniscsiusr_common.h"
+#include "rfc.h"
+#include "idbm.h"
+
+#define PORTAL_GROUP_TAG_UNKNOWN	-1
+/* q depths */
+#define CMDS_MAX			128
+#define QUEUE_DEPTH			32
+
+/* system */
+#define XMIT_THREAD_PRIORITY		-20
+
+/* login retries */
+#define DEF_INITIAL_LOGIN_RETRIES_MAX	4
+
+/* autoscan enabled */
+#define DEF_INITIAL_SCAN		INIT_SCAN_AUTO
+
+/*
+ * Default initiator settings. These may not be the same as
+ * in the RFC. See libopeniscsiusr/libopeniscsiusr_rfc.h for those.
+ */
+/* timeouts in seconds */
+#define DEF_LOGIN_TIMEO			30
+#define DEF_LOGOUT_TIMEO		15
+#define DEF_NOOP_OUT_INTERVAL		5
+#define DEF_NOOP_OUT_TIMEO		5
+#define DEF_REPLACEMENT_TIMEO		120
+
+#define DEF_ABORT_TIMEO			15
+#define DEF_LU_RESET_TIMEO		30
+#define DEF_TGT_RESET_TIMEO		30
+#define DEF_HOST_RESET_TIMEO		60
+
+/* default window size */
+#define TCP_WINDOW_SIZE			(512 * 1024)
+
+/* data and segment lengths in bytes */
+#define DEF_INI_FIRST_BURST_LEN		262144
+#define DEF_INI_MAX_BURST_LEN		16776192
+#define DEF_INI_MAX_RECV_SEG_LEN	262144
+
+#define DEFAULT_TRANSPORT	"tcp"
+#define DEFAULT_IFACENAME	"default"
+#define DEFAULT_NETDEV		"default"
+#define DEFAULT_IPADDRESS	"default"
+#define DEFAULT_HWADDRESS	"default"
+
+void __DLL_LOCAL _default_node(struct iscsi_node *node);
+
+#endif /* End of __ISCSI_USR_DEFAULT_H__ */

commit baa0cb45cfcf10a81283c191b0b236cd1a2f66ee

Restore space in node-mode level 0 output
As part of the libopeniscsiusr update, the output
of "iscsiadm -m node" dropped a space that needs to
be between the portal and the target, for proper
parsing.

Fixes: 87ea50a1c3a97

usr/iscsiadm.c
@@ -683,7 +683,7 @@ static int login_portals(struct node_rec *pattern_rec)
 
 static void print_node_flat(struct iscsi_node *node)
 {
-		printf("%s,%" PRIu16 "%s\n",
+		printf("%s,%" PRIu16 " %s\n",
 		       iscsi_node_portal_get(node),
 		       iscsi_node_tpgt_get(node),
 		       iscsi_node_target_name_get(node));