open-iscsi/open-iscsi

commit b532ad67d495d42026165a26515c645995d23f18

open-iscsi: Clean user_param list when process exit
Both in iscsiadm and iscsistart, a list is initialized to management
struct iscsi_param which allocated by idbm_alloc_user_param().
While both of iscsiadm and iscsistart did not free these iscsi_param
when process exit, this patch add a free flow when process exit.

Signed-off-by: Wenchao Hao <haowenchao@huawei.com>
Signed-off-by: Zhiqiang Liu <liuzhiqiang26@huawei.com>
Signed-off-by: Wu Bo <wubo40@huawei.com>

usr/iscsiadm.c
@@ -4075,6 +4075,10 @@ free_ifaces:
 		list_del(&iface->list);
 		free(iface);
 	}
+	list_for_each_entry(param, &params, list) {
+		list_del(&param->list);
+		idbm_free_user_param(param);
+	}
 	free_transports();
 	sysfs_cleanup();
 	return rc;

usr/idbm.c
@@ -2903,6 +2903,20 @@ free_param:
 	return NULL;
 }
 
+void idbm_free_user_param(struct user_param *param)
+{
+	if (!param)
+		return;
+
+	if (param->name)
+		free(param->name);
+
+	if (param->value)
+		free(param->value);
+
+	free(param);
+}
+
 int idbm_node_set_rec_from_param(struct list_head *params, node_rec_t *rec,
 				 int verify)
 {

usr/idbm.h
@@ -153,6 +153,7 @@ extern int idbm_node_set_rec_from_param(struct list_head *params,
 extern int idbm_node_set_param(void *data, node_rec_t *rec);
 extern int idbm_discovery_set_param(void *data, discovery_rec_t *rec);
 struct user_param *idbm_alloc_user_param(char *name, char *value);
+void idbm_free_user_param(struct user_param *param);
 extern void idbm_node_setup_defaults(node_rec_t *rec);
 extern struct node_rec *idbm_find_rec_in_list(struct list_head *rec_list,
 					      char *targetname, char *addr,

usr/iscsistart.c
@@ -347,6 +347,7 @@ int main(int argc, char *argv[])
 	struct boot_context *context, boot_context;
 	struct sigaction sa_old;
 	struct sigaction sa_new;
+	struct user_param *param;
 	int control_fd, mgmt_ipc_fd, err;
 	pid_t pid;
 
@@ -541,6 +542,10 @@ int main(int argc, char *argv[])
 	mgmt_ipc_close(mgmt_ipc_fd);
 	free_initiator();
 	sysfs_cleanup();
+	list_for_each_entry(param, &user_params, list) {
+		list_del(&param->list);
+		idbm_free_user_param(param);
+	}
 
 	log_debug(1, "iscsi child done");
 	return 0;

commit 76a5ebf955702f676a5ea5f7b43bb8fb436edc40

Fix iscsiadm segfault when exiting
Commit b532ad67d495d added some cleanup code
to iscsiadm right before it exits, but it
used a list_for_each_entry() to iterate through
a list was being deleted, when it should use
list_for_each_entry_safe().

Fixes: b532ad67d495d

usr/iscsiadm.c
@@ -3582,11 +3582,11 @@ main(int argc, char **argv)
 	struct sigaction sa_old;
 	struct sigaction sa_new;
 	LIST_HEAD(ifaces);
-	struct iface_rec *iface = NULL, *tmp;
+	struct iface_rec *iface = NULL, *tmp_iface;
 	struct node_rec *rec = NULL;
 	uint32_t host_no =  MAX_HOST_NO + 1;
 	uint64_t index = ULLONG_MAX;
-	struct user_param *param;
+	struct user_param *param, *tmp_param;
 	LIST_HEAD(params);
 	struct iscsi_context *ctx = NULL;
 	int librc = LIBISCSI_OK;
@@ -4070,11 +4070,11 @@ out:
 		free(rec);
 	iscsi_sessions_free(ses, se_count);
 	idbm_terminate();
-	list_for_each_entry_safe(iface, tmp, &ifaces, list) {
+	list_for_each_entry_safe(iface, tmp_iface, &ifaces, list) {
 		list_del(&iface->list);
 		free(iface);
 	}
-	list_for_each_entry(param, &params, list) {
+	list_for_each_entry_safe(param, tmp_param, &params, list) {
 		list_del(&param->list);
 		idbm_free_user_param(param);
 	}