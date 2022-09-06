580d386b63bb72dfe798b6e556d5e673ef80a73c
Lee Duncan <lduncan@suse.com>
2022-09-06 13:19:19 -0700

    Add framework to support building using meson.
    This adds the ability to use meson/ninja to build open-iscsi
    and iscsiuio, rather than the current system that uses 'autoconf'
    for iscsiuio and uses 'make' for everything else.
    
    The old make/autoconf system is left in place, for now, but
    deprecated, including a warning about that when running 'make all'
    or 'make user' from the top-level.

--- iscsiuio/src/unix/build_date.sh
+++ iscsiuio/src/unix/build_date.sh
@@ -6,19 +6,26 @@
 #
 
 THIS_CMD="build_date"
+SOURCE_DATE_EPOCH=""
 
 usage()
 {
-    echo "usage: $THIS_CMD -c OUTSRC -- generate date C code file OUTSRC"
-    echo "   or: $THIS_CMD -i OUTHDR -- generate date C code include file OUTHDR"
+    echo "Usage: $THIS_CMD [OPTIONS]"
+    echo "Where OPTIONS are from:"
+    echo "  -c OUT_SOURCE         create C source file OUT_SOURCE with the date"
+    echo "  -i OUT_HEADER         create C include file OUT_HEADER for the date file"
+    echo "  -S EPOCH_DATE_NUMBER  use '--date=@EPOCH_DATE_NUMBER' to set date (repeatable builds)"
+    echo "Also sets EPOCH date number from SOURCE_DATE_EPOCH if set in the environment"
 }
 
 generate_source_file()
 {
     outfile="$1"
     if [ -n "$SOURCE_DATE_EPOCH" ] ; then
+	echo "SOURCE_DATE_EPOCH set!" 1>&2
 	echo 'char *build_date = "'`LC_ALL=C.UTF-8 date --date=@$SOURCE_DATE_EPOCH -u`'";' >"$outfile"
     else
+	echo "SOURCE_DATE_EPOCH not set :(" 1>&2
 	echo 'char *build_date = "'`date`'";' >"$outfile"
     fi
 }
@@ -29,13 +36,23 @@ generate_include_file()
     echo 'extern char *build_date;' >"$outfile"
 }
 
+do_source=
+do_include=
 
-while getopts :c:i:h opt; do
+while getopts :c:i:s:h opt; do
     case "$opt" in
-    c) generate_source_file $OPTARG; exit 0 ;;
-    i) generate_include_file $OPTARG; exit 0 ;;
+    c) do_source="$OPTARG" ;;
+    i) do_include="$OPTARG" ;;
+    s) SOURCE_DATE_EPOCH="$OPTARG" ;;
     h) usage; exit 0 ;;
     ?) echo "unknown option: $opt" 1>&2; usage; exit 1 ;; 
     *) echo "huh???"
     esac
 done
+
+if [ -n "$do_source" ]; then
+   generate_source_file $do_source
+fi
+if [ -n "$do_include" ]; then
+    generate_include_file $do_include
+fi
