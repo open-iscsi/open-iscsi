1391bcce0375759aab930bdf3b47003c7b2267c0
Lee Duncan <lduncan@suse.com>
2022-09-06 08:29:01 -0700

    build: gaurd against ISCSI_VERSION_STR not being set
    Just in case somebody tries to build without a
    vesion string (supplied by the build system).

--- usr/version.h
+++ usr/version.h
@@ -5,7 +5,12 @@
  * iSCSI tools version.
  * This may not be the same value as the kernel versions because
  * some other maintainer could merge a patch without going through us
+ *
+ * Version string should be set by build system, or we have a problem
  */
+#ifndef	ISCSI_VERSION_STR
+#error ISCSI_VERSION_STR not set!
+#endif
 #define ISCSI_VERSION_FILE	"/sys/module/scsi_transport_iscsi/version"
 
 #endif
