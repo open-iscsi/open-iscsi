open-iscsi/open-iscsi

commit 9d99b50a88ef1bc74dfdbda19d89594c2cac0945

Set default 'startup' to 'onboot' for FW nodes
When "iscsiadm -m discovery -t fw" is ran, nodes are
created/updated in the node DB for each firmware target
found, but the 'startup' mode values are left as 'manual',
when in fact firmware nodes are treated as if this
value is 'onboot'. So to keep the node DB in sync with
behavior, set these to 'onboot' by default. This
should *not* effect any other functionality.

usr/idbm.c
@@ -2458,6 +2458,12 @@ int idbm_add_node(node_rec_t *newrec, discovery_rec_t *drec, int overwrite)
 		rc = idbm_delete_node(&rec);
 		if (rc)
 			goto unlock;
+
+		if (drec->type == DISCOVERY_TYPE_FW) {
+			log_debug(8, "setting firmware node 'startup' to 'onboot'");
+			newrec->startup = ISCSI_STARTUP_ONBOOT;
+			newrec->conn[0].startup = ISCSI_STARTUP_ONBOOT;
+		}
 		log_debug(7, "overwriting existing record");
 	} else
 		log_debug(7, "adding new DB record");

commit 963f8f96080a2e9b6340bd9e0a454ee284ce96d5

Fix firmware targets startup to always be "onboot"
Commit 9d99b50a88ef ("Set default 'startup' to 'onboot' for FW nodes")
added code to always set "onboot" for the startup mode for firmware
targets, but it did so only for when targets database entries are
updated, not when they are created.

This commit fixes that by moving the code that sets startup entries
to "onboot" so that it happens for all firmware DB entries.

Fixes: 9d99b50a88ef ("Set default 'startup' to 'onboot' for FW nodes")

usr/idbm.c
@@ -2569,16 +2569,16 @@ int idbm_add_node(node_rec_t *newrec, discovery_rec_t *drec, int overwrite)
 		if (rc)
 			goto unlock;
 
-		if (drec->type == DISCOVERY_TYPE_FW) {
-			log_debug(8, "setting firmware node 'startup' to 'onboot'");
-			newrec->startup = ISCSI_STARTUP_ONBOOT;
-			newrec->conn[0].startup = ISCSI_STARTUP_ONBOOT;
-		}
 		log_debug(7, "overwriting existing record");
 	} else
 		log_debug(7, "adding new DB record");
 
 	if (drec) {
+		if (drec->type == DISCOVERY_TYPE_FW) {
+			log_debug(8, "setting firmware node 'startup' to 'onboot'");
+			newrec->startup = ISCSI_STARTUP_ONBOOT;
+			newrec->conn[0].startup = ISCSI_STARTUP_ONBOOT;
+		}
 		newrec->disc_type = drec->type;
 		newrec->disc_port = drec->port;
 		strcpy(newrec->disc_address, drec->address);