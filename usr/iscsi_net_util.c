open-iscsi/open-iscsi

commit 72c2b97c19a2d0710b38802d50813feb682278f7

IPv6 support for iBFT iSCSI boot (#493)
* Avoid reassigning a char pointer variable to a newly allocated string

Introduce two separate variables to clearly distinguish their usage.

Signed-off-by: Chengen Du <chengen.du@canonical.com>

* IPv6 support for iBFT iSCSI boot

Use IPv4-specific APIs during NIC setup based on iBFT information,
regardless of the IP version, which leads to the following error
in IPv6-only environments:

Setting up software interface ens300f0np0
iscsistart: Invalid or missing ipaddr in fw entry
Setting up software interface ens300f0np0 failed

To address this, the process now determines the IP version first
and calls the appropriate functions to handle NIC setup.

Signed-off-by: Chengen Du <chengen.du@canonical.com>

---------

Signed-off-by: Chengen Du <chengen.du@canonical.com>

usr/iscsi_net_util.c
@@ -32,6 +32,8 @@
 #include <linux/if_vlan.h>
 #include <net/if_arp.h>
 #include <linux/if_ether.h>
+#include <netdb.h>
+#include <linux/ipv6.h>
 
 #include "sysdeps.h"
 #include "ethtool-copy.h"
@@ -234,8 +236,57 @@ static char *find_vlan_dev(char *netdev, int vlan_id) {
 	return vlan;
 }
 
+int net_get_ip_version(char *ip)
+{
+	struct addrinfo hint;
+	struct addrinfo *res = NULL;
+	int ret;
+
+	memset(&hint, 0, sizeof hint);
+	hint.ai_family = PF_UNSPEC;
+	hint.ai_flags = AI_NUMERICHOST;
+	ret = getaddrinfo(ip, NULL, &hint, &res);
+	if (ret) {
+		log_error("Invalid IP address %s\n", ip);
+		return EINVAL;
+	}
+
+	ret = res->ai_family;
+	freeaddrinfo(res);
+	return ret;
+}
+
+static int net_bringup_netdev(int sock, char *physdev, char *netdev)
+{
+	struct ifreq ifr;
+
+	if (physdev) {
+		/* Bring up interface */
+		memset(&ifr, 0, sizeof(ifr));
+		strlcpy(ifr.ifr_name, physdev, IFNAMSIZ);
+		ifr.ifr_flags = IFF_UP | IFF_RUNNING;
+		if (ioctl(sock, SIOCSIFFLAGS, &ifr) < 0) {
+			log_error("Could not bring up netdev %s (err %d - %s)",
+				  physdev, errno, strerror(errno));
+			return errno;
+		}
+	}
+
+	/* Bring up interface */
+	memset(&ifr, 0, sizeof(ifr));
+	strlcpy(ifr.ifr_name, netdev, IFNAMSIZ);
+	ifr.ifr_flags = IFF_UP | IFF_RUNNING;
+	if (ioctl(sock, SIOCSIFFLAGS, &ifr) < 0) {
+		log_error("Could not bring up netdev %s (err %d - %s)",
+			  netdev, errno, strerror(errno));
+		return errno;
+	}
+
+	return 0;
+}
+
 /**
- * net_setup_netdev - bring up NIC
+ * net_setup_netdev_ipv4 - bring up NIC for IPv4
  * @netdev: network device name
  * @local: ip address for netdev
  * @mask: net mask
@@ -246,8 +297,8 @@ static char *find_vlan_dev(char *netdev, int vlan_id) {
  * Bring up required NIC and use routing
  * to force iSCSI traffic through correct NIC.
  */
-int net_setup_netdev(char *netdev, char *local_ip, char *mask, char *gateway,
-		     char *vlan, char *remote_ip, int needs_bringup)
+int net_setup_netdev_ipv4(char *netdev, char *local_ip, char *mask, char *gateway,
+			  char *vlan, char *remote_ip, int needs_bringup)
 {
 	struct sockaddr_in sk_ipaddr = { .sin_family = AF_INET };
 	struct sockaddr_in sk_netmask = { .sin_family = AF_INET };
@@ -257,6 +308,8 @@ int net_setup_netdev(char *netdev, char *local_ip, char *mask, char *gateway,
 	struct rtentry rt;
 	struct ifreq ifr;
 	char *physdev = NULL;
+	char *vlandev = NULL;
+	char *targetdev;
 	int sock;
 	int ret;
 	int vlan_id;
@@ -266,14 +319,16 @@ int net_setup_netdev(char *netdev, char *local_ip, char *mask, char *gateway,
 		return EINVAL;
 	}		
 
+	targetdev = netdev;
 	vlan_id = atoi(vlan);
 
 	if (vlan_id != 0) {
-		physdev = netdev;
-		netdev = find_vlan_dev(physdev, vlan_id);
+		vlandev = find_vlan_dev(physdev, vlan_id);
+		physdev = targetdev;
+		targetdev = vlandev;
 	}
 
-	if (vlan_id && !netdev) {
+	if (vlan_id && !vlandev) {
 		/* TODO: create vlan if not found */
 		log_error("No matching vlan found for fw entry.");
 		return EINVAL;
@@ -312,48 +367,28 @@ int net_setup_netdev(char *netdev, char *local_ip, char *mask, char *gateway,
 
 	/* Only set IP/NM if this is a new interface */
 	if (needs_bringup) {
-
-		if (physdev) {
-			/* Bring up interface */
-			memset(&ifr, 0, sizeof(ifr));
-			strlcpy(ifr.ifr_name, physdev, IFNAMSIZ);
-			ifr.ifr_flags = IFF_UP | IFF_RUNNING;
-			if (ioctl(sock, SIOCSIFFLAGS, &ifr) < 0) {
-				log_error("Could not bring up netdev %s (err %d - %s)",
-					  physdev, errno, strerror(errno));
-				ret = errno;
-				goto done;
-			}
-		}
-
-		/* Bring up interface */
-		memset(&ifr, 0, sizeof(ifr));
-		strlcpy(ifr.ifr_name, netdev, IFNAMSIZ);
-		ifr.ifr_flags = IFF_UP | IFF_RUNNING;
-		if (ioctl(sock, SIOCSIFFLAGS, &ifr) < 0) {
-			log_error("Could not bring up netdev %s (err %d - %s)",
-				  netdev, errno, strerror(errno));
-			ret = errno;
+		ret = net_bringup_netdev(sock, physdev, targetdev);
+		if (ret)
 			goto done;
-		}
+
 		/* Set IP address */
 		memset(&ifr, 0, sizeof(ifr));
-		strlcpy(ifr.ifr_name, netdev, IFNAMSIZ);
+		strlcpy(ifr.ifr_name, targetdev, IFNAMSIZ);
 		memcpy(&ifr.ifr_addr, &sk_ipaddr, sizeof(struct sockaddr));
 		if (ioctl(sock, SIOCSIFADDR, &ifr) < 0) {
 			log_error("Could not set ip for %s (err %d - %s)",
-				  netdev, errno, strerror(errno));
+				  targetdev, errno, strerror(errno));
 			ret = errno;
 			goto done;
 		}
 
 		/* Set netmask */
 		memset(&ifr, 0, sizeof(ifr));
-		strlcpy(ifr.ifr_name, netdev, IFNAMSIZ);
+		strlcpy(ifr.ifr_name, targetdev, IFNAMSIZ);
 		memcpy(&ifr.ifr_addr, &sk_netmask, sizeof(struct sockaddr));
 		if (ioctl(sock, SIOCSIFNETMASK, &ifr) < 0) {
 			log_error("Could not set ip for %s (err %d - %s)",
-				  netdev, errno, strerror(errno));
+				  targetdev, errno, strerror(errno));
 			ret = errno;
 			goto done;
 		}
@@ -364,7 +399,7 @@ int net_setup_netdev(char *netdev, char *local_ip, char *mask, char *gateway,
 	memcpy(&rt.rt_dst, &sk_tgt_ipaddr, sizeof(sk_tgt_ipaddr));
 	memcpy(&rt.rt_genmask, &sk_hostmask, sizeof(sk_hostmask));
 	rt.rt_flags = RTF_UP | RTF_HOST;
-	rt.rt_dev = netdev;
+	rt.rt_dev = targetdev;
 
 	if ((sk_tgt_ipaddr.sin_addr.s_addr & sk_netmask.sin_addr.s_addr) == 
 		(sk_ipaddr.sin_addr.s_addr & sk_netmask.sin_addr.s_addr)) {
@@ -372,7 +407,7 @@ int net_setup_netdev(char *netdev, char *local_ip, char *mask, char *gateway,
 		if (ioctl(sock, SIOCADDRT, &rt) < 0) {
 			if (errno != EEXIST) {
 				log_error("Could not set ip for %s "
-					  "(err %d - %s)", netdev,
+					  "(err %d - %s)", targetdev,
 					   errno, strerror(errno));
 				ret = errno;
 				goto done;
@@ -384,7 +419,7 @@ int net_setup_netdev(char *netdev, char *local_ip, char *mask, char *gateway,
 		if (!inet_pton(AF_INET, gateway, &sk_gateway.sin_addr)) {
 			log_error("Invalid or missing gateway for %s "
 				  "(err %d - %s)",
-				  netdev, errno, strerror(errno));
+				  targetdev, errno, strerror(errno));
 			ret = errno;
 			goto done;
 		}
@@ -392,7 +427,7 @@ int net_setup_netdev(char *netdev, char *local_ip, char *mask, char *gateway,
 		if (ioctl(sock, SIOCADDRT, &rt) < 0) {
 			if (errno != EEXIST) {
 				log_error("Could not set gateway for %s "
-					  "(err %d - %s)", netdev,
+					  "(err %d - %s)", targetdev,
 					  errno, strerror(errno));
 				ret = errno;
 				goto done;
@@ -404,8 +439,133 @@ int net_setup_netdev(char *netdev, char *local_ip, char *mask, char *gateway,
 done:
 	if (sock >= 0)
 		close(sock);
-	if (vlan_id)
-		free(netdev);
+	if (vlandev)
+		free(vlandev);
+	return ret;
+}
+
+/**
+ * net_setup_netdev_ipv6 - bring up NIC for IPv6
+ * @netdev: network device name
+ * @local: ip address for netdev
+ * @prefix: prefix length
+ * @gateway: gateway
+ * @vlan: vlan ID
+ * @remote_ip: target portal ip
+ * @needs_bringup: bool indicating if the netdev needs to be started
+ *
+ * Bring up required NIC and use routing
+ * to force iSCSI traffic through correct NIC.
+ */
+int net_setup_netdev_ipv6(char *netdev, char *local_ip, int prefix, char *gateway,
+			  char *vlan, char *remote_ip, int needs_bringup)
+{
+	struct sockaddr_in6 sk_ipaddr = { .sin6_family = AF_INET6 };
+	struct sockaddr_in6 sk_gateway = { .sin6_family = AF_INET6 };
+	struct sockaddr_in6 sk_tgt_ipaddr = { .sin6_family = AF_INET6 };
+	struct in6_ifreq ifr6;
+	struct in6_rtmsg rtmsg;
+	char *physdev = NULL;
+	char *vlandev = NULL;
+	char *targetdev;
+	int sock;
+	int ret;
+	int vlan_id;
+	int ifindex;
+
+	if (!strlen(netdev)) {
+		log_error("No netdev name in fw entry.\n");
+		return EINVAL;
+	}
+	targetdev = netdev;
+
+	vlan_id = atoi(vlan);
+	if (vlan_id) {
+		vlandev = find_vlan_dev(physdev, vlan_id);
+		physdev = targetdev;
+		targetdev = vlandev;
+	}
+	if (vlan_id && !vlandev) {
+		log_error("No matching VLAN found for fw entry.\n");
+		return EINVAL;
+	}
+
+	/* Create socket for making networking changes */
+	if ((sock = socket(AF_INET6, SOCK_DGRAM, 0)) == -1) {
+		log_error("Could not open socket to manage network (err %d - %s)\n",
+			  errno, strerror(errno));
+		ret = errno;
+		goto done;
+	}
+
+	/* Bring up NIC with correct address */
+	if (!inet_pton(AF_INET6, local_ip, &sk_ipaddr.sin6_addr)) {
+		log_error("Invalid or missing IPv6 address in fw entry.\n");
+		ret = EINVAL;
+		goto done;
+	}
+
+	if (!inet_pton(AF_INET6, remote_ip, &sk_tgt_ipaddr.sin6_addr)) {
+		log_error("Invalid or missing target ipaddr in fw entry.\n");
+		ret = EINVAL;
+		goto done;
+	}
+
+	if (!inet_pton(AF_INET6, gateway, &sk_gateway.sin6_addr)) {
+		log_error("Invalid or missing gateway in fw entry.\n");
+		ret = EINVAL;
+		goto done;
+	}
+
+	ifindex = if_nametoindex(targetdev);
+	if (!ifindex) {
+		log_error("Could not get index for %s (err %d - %s)\n",
+			  targetdev, errno, strerror(errno));
+		ret = errno;
+		goto done;
+	}
+
+	/* Only set IP if this is a new interface */
+	if (needs_bringup) {
+		ret = net_bringup_netdev(sock, physdev, targetdev);
+		if (ret)
+			goto done;
+
+		/* Set IPv6 address */
+		memset(&ifr6, 0, sizeof(ifr6));
+		memcpy(&ifr6.ifr6_addr, &sk_ipaddr.sin6_addr, sizeof(struct in6_addr));
+		ifr6.ifr6_ifindex = ifindex;
+		ifr6.ifr6_prefixlen = prefix;
+		if (ioctl(sock, SIOCSIFADDR, &ifr6) < 0) {
+			log_error("Could not set IPv6 address for %s (err %d - %s)\n",
+				  targetdev, errno, strerror(errno));
+			ret = errno;
+			goto done;
+		}
+	}
+
+	/* Set static route to target via this interface */
+	memset(&rtmsg, 0, sizeof(rtmsg));
+	memcpy(&rtmsg.rtmsg_dst, &sk_tgt_ipaddr.sin6_addr, sizeof(struct in6_addr));
+	memcpy(&rtmsg.rtmsg_gateway, &sk_gateway.sin6_addr, sizeof(struct in6_addr));
+	rtmsg.rtmsg_dst_len = 128;
+	rtmsg.rtmsg_ifindex = ifindex;
+	rtmsg.rtmsg_flags = RTF_GATEWAY | RTF_UP;
+	if (ioctl(sock, SIOCADDRT, &rtmsg) < 0) {
+		if (errno != EEXIST) {
+			log_error("Could not add IPv6 route for %s (err %d - %s)\n",
+				  targetdev, errno, strerror(errno));
+			ret = errno;
+			goto done;
+		}
+	}
+	ret = 0;
+
+done:
+	if (sock >= 0)
+		close(sock);
+	if (vlandev)
+		free(vlandev);
 	return ret;
 }
 

include/iscsi_net_util.h
@@ -11,9 +11,13 @@
 
 extern int net_get_transport_name_from_netdev(char *netdev, char *transport);
 extern int net_get_netdev_from_hwaddress(char *hwaddress, char *netdev);
-extern int net_setup_netdev(char *netdev, char *local_ip, char *mask,
-			    char *gateway, char *vlan, char *remote_ip,
-			    int needs_bringup);
+extern int net_get_ip_version(char *ip);
+extern int net_setup_netdev_ipv4(char *netdev, char *local_ip, char *mask,
+				 char *gateway, char *vlan, char *remote_ip,
+				 int needs_bringup);
+extern int net_setup_netdev_ipv6(char *netdev, char *local_ip, int prefix,
+				 char *gateway, char *vlan, char *remote_ip,
+				 int needs_bringup);
 extern int net_ifup_netdev(char *netdev);
 
 #endif

usr/fwparam_ibft/fw_entry.c
@@ -73,12 +73,22 @@ int fw_setup_nics(void)
 				needs_bringup = 1;
 		}
 		if (net_get_transport_name_from_netdev(context->iface, transport)) {
+			int ip_ver = net_get_ip_version(context->ipaddr);
+
 			/* Setup software NIC, */
 			printf("Setting up software interface %s\n", context->iface);
-			err = net_setup_netdev(context->iface, context->ipaddr,
-								   context->mask, context->gateway,
-								   context->vlan,
-								   context->target_ipaddr, needs_bringup);
+			if (ip_ver == AF_INET)
+				err = net_setup_netdev_ipv4(context->iface, context->ipaddr,
+						context->mask, context->gateway,
+						context->vlan, context->target_ipaddr,
+						needs_bringup);
+			else if (ip_ver == AF_INET6)
+				err = net_setup_netdev_ipv6(context->iface, context->ipaddr,
+						context->prefix, context->gateway,
+						context->vlan, context->target_ipaddr,
+						needs_bringup);
+			else
+				err = ip_ver;
 			if (err) {
 				printf("Setting up software interface %s failed\n",
 						context->iface);

commit 932f61d3729043acada449512c2e5f36208af8db

iscsi_net_util: fix broken VLAN support in find_vlan_dev
Commit 72c2b97c introduced a bug by passing physdev to find_vlan_dev()
before it was initialized (physdev was set after the call instead of
before). This was then made worse by commit 996269f which removed the
netdev parameter entirely, leaving if_hwaddr.ifr_name uninitialized.

This fix:
- Restores the netdev parameter to find_vlan_dev()
- Re-adds the strlcpy() to initialize if_hwaddr.ifr_name
- Fixes the call order to set physdev before calling find_vlan_dev()
  in both net_setup_netdev_ipv4() and net_setup_netdev_ipv6()

Signed-off-by: Chris Leech <cleech@redhat.com>

usr/iscsi_net_util.c
@@ -185,7 +185,7 @@ free_ifni:
 	return 0;
 }
 
-static char *find_vlan_dev(int vlan_id) {
+static char *find_vlan_dev(char *netdev, int vlan_id) {
 	struct ifreq if_hwaddr;
 	struct ifreq vlan_hwaddr;
 	struct vlan_ioctl_args vlanrq = { .cmd = GET_VLAN_VID_CMD, };
@@ -199,6 +199,7 @@ static char *find_vlan_dev(int vlan_id) {
 		return NULL;
 	}
 
+	strlcpy(if_hwaddr.ifr_name, netdev, IFNAMSIZ);
 	ioctl(sockfd, SIOCGIFHWADDR, &if_hwaddr);
 
 	if (if_hwaddr.ifr_hwaddr.sa_family != ARPHRD_ETHER) {
@@ -322,8 +323,8 @@ int net_setup_netdev_ipv4(char *netdev, char *local_ip, char *mask, char *gatewa
 	vlan_id = atoi(vlan);
 
 	if (vlan_id != 0) {
-		vlandev = find_vlan_dev(vlan_id);
 		physdev = targetdev;
+		vlandev = find_vlan_dev(physdev, vlan_id);
 		targetdev = vlandev;
 	}
 
@@ -480,8 +481,8 @@ int net_setup_netdev_ipv6(char *netdev, char *local_ip, int prefix, char *gatewa
 
 	vlan_id = atoi(vlan);
 	if (vlan_id) {
-		vlandev = find_vlan_dev(vlan_id);
 		physdev = targetdev;
+		vlandev = find_vlan_dev(physdev, vlan_id);
 		targetdev = vlandev;
 	}
 	if (vlan_id && !vlandev) {