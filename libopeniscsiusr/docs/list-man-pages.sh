e2be87444108d90cf9c4439f4d66c3b9ebb900ea
Natanael Copa <ncopa@alpinelinux.org>
2026-09-01 11:03:18 +0200

    libopeniscsiusr: make list-man-pages.sh POSIX compatible
    - use /bin/sh
    - preserve filename boundaries and print sed output directly
    - make it pass shellcheck

--- libopeniscsiusr/docs/list-man-pages.sh
+++ libopeniscsiusr/docs/list-man-pages.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 # list man pages found given one or more ??? passed in
 #
@@ -6,16 +6,8 @@
 #  https://github.com/linux-nvme/libnvme:doc/list-man-pages.sh
 #
 
-for file in $@; do
-    for func in $(sed -n 's/ \* \([a-z_][a-z_0-9]*\)() -.*/\1/p' $file); do
-	echo ${func}
-    done
-
-    for struct in $(sed -n 's/ \* struct \([a-z_]*\) -.*/\1/p' $file); do
-	echo ${struct}
-    done
-
-    for enum in $(sed -n 's/ \* enum \([a-z_]*\) -.*/\1/p' $file); do
-	echo ${enum}
-    done
+for file do
+    sed -n 's/ \* \([a-z_][a-z_0-9]*\)() -.*/\1/p' "$file"
+    sed -n 's/ \* struct \([a-z_]*\) -.*/\1/p' "$file"
+    sed -n 's/ \* enum \([a-z_]*\) -.*/\1/p' "$file"
 done
