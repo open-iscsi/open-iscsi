4f573e5817fd9ab3c8441c74c1ac8c1672d8708d
Natanael Copa <ncopa@alpinelinux.org>
2026-09-01 11:06:04 +0200

    libopeniscsiusr: make runtest.sh POSIX compatible
    - use POSIX script-directory detection
    - pass valgrind options as separate arguments
    - make it pass shellcheck

--- libopeniscsiusr/tests/runtest.sh
+++ libopeniscsiusr/tests/runtest.sh
@@ -1,30 +1,32 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ "CHK$TESTS"  == "CHK" ];then
+if [ -z "${TESTS:-}" ]; then
     echo "# No test cases defined"
     exit 1
 fi
 
 VALGRIND_ERR_RC=2
-VALGRIND_OPTS="--quiet --leak-check=full \
-               --show-reachable=no --show-possibly-lost=no \
-               --trace-children=yes --error-exitcode=$VALGRIND_ERR_RC"
+case "$0" in
+*/*) TEST_DIR=${0%/*} ;;
+*) TEST_DIR=. ;;
+esac
+TEST_DIR=$(cd "$TEST_DIR" || exit; pwd)
 
-TEST_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-for TEST in $TESTS; do
+for test_path in $TESTS; do
     echo
-    TEST=$(basename $TEST)
+    TEST=${test_path##*/}
     echo "## RUN  '$TEST'"
-    valgrind $VALGRIND_OPTS $TEST_DIR/$TEST
+    valgrind --quiet --leak-check=full --show-reachable=no \
+        --show-possibly-lost=no --trace-children=yes \
+        --error-exitcode="$VALGRIND_ERR_RC" "$TEST_DIR/$TEST"
     rc=$?
-    if [ $rc -ne 0 ]; then
-        if [ $rc -eq $VALGRIND_ERR_RC ];then
+    if [ "$rc" -ne 0 ]; then
+        if [ "$rc" -eq "$VALGRIND_ERR_RC" ]; then
             echo
             echo "### Found memory leak"
-            exit $rc
+            exit "$rc"
         fi
-        exit $rc
+        exit "$rc"
     fi
     echo "## PASS '$TEST'"
 done
